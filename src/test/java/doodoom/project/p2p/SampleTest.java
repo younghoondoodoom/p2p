f1b2e6d205a8837be4021b4b7da91dc95d86b11f
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-23 16:53:44 +0900

    test : add test

---
+++ src/test/java/doodoom/project/p2p/SampleTest.java
@@ -0,0 +1,18 @@
+package doodoom.project.p2p;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() throws Exception {
+        assertThat("test").isEqualTo("test");
+    }
+
+    @Test
+    public void sampleTest2() throws Exception {
+        assertThat("test").isEqualTo("test");
+    }
+}
